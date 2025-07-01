0b152bb70eee5d038bcde92e939de96a68802d6d
Vijay Appadorai <vijay.appadorai@gmail.com>
2025-07-01 14:05:57 +1000

    Update run_evaluation.py
    Added all the tests in the same location

--- run_evaluation.py
+++ run_evaluation.py
@@ -3,7 +3,7 @@ from model_evaluation import evaluate_model
 from models_wrapper import load_summarizer  # or load_sentiment_pipeline
 
 if __name__ == "__main__":
-    model_name = "eprasad/t5-small-llama70b-distill-summarization"
+    model_name = "t5-small"
     summarizer = load_summarizer(model_name)
 
     dataset = load_dataset("cnn_dailymail", "3.0.0", split="test[:100]")
@@ -16,5 +16,38 @@ if __name__ == "__main__":
         model_name=model_name,
         inputs=inputs,
         references=references,
-        output_csv="eval1.csv"
+        output_csv="withrouge_recall_calculations.csv"
     )
+
+    model_name = "eprasad/t5-small-llama70b-distill-summarization"
+    summarizer = load_summarizer(model_name)
+    evaluate_model(
+        skill_type='summarization',
+        trained_model=summarizer,
+        model_name=model_name,
+        inputs=inputs,
+        references=references,
+        output_csv="withrouge_recall_calculations.csv"
+    )
+    model_name = "eprasad/t5-small-qwen3-distill-summarization"
+    summarizer = load_summarizer(model_name)
+    evaluate_model(
+        skill_type='summarization',
+        trained_model=summarizer,
+        model_name=model_name,
+        inputs=inputs,
+        references=references,
+        output_csv="withrouge_recall_calculations.csv"
+    )
+    model_name = "AhilanPonnusamy/distilled-t5small-summarizer"
+    summarizer = load_summarizer(model_name)
+    evaluate_model(
+        skill_type='summarization',
+        trained_model=summarizer,
+        model_name=model_name,
+        inputs=inputs,
+        references=references,
+        output_csv="withrouge_recall_calculations.csv"
+    )
+
+
